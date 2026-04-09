bf290f3460fa1a6a0da92437c8e045e4951d7eb5
ArijitBanerjee <arijitbanerjee873gmail.com>
2026-04-09 20:25:40 +0530

    fix minor isues

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,23 +1,32 @@
 import express from "express";
 import connectDB from "./Config/DbConfig.js";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import router from "./routes/routes.js";
-import cors from 'cors'
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const app=express();
-app.use(express.json());
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(cors());
 
 connectDB();
 
-app.use(cors());
-app.use("/api",router);
+app.use("/api", router);
 
-app.get("/", (req, res) => {
-    res.send("SnapShare API is running perfectly! 🎉");
+app.use(express.static(path.join(__dirname, "../Client/dist")));
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../Client/dist/index.html"));
 });
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
