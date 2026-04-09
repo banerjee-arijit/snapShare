e7f9797189f868665e3b8ae39a3719e32f00b0c7
ArijitBanerjee <arijitbanerjee873gmail.com>
2026-04-09 19:11:18 +0530

    change folder name

--- Server/src/server.js
+++ Server/src/server.js
@@ -11,7 +11,12 @@ dotenv.config();
 
 connectDB();
 
-app.use(cors())
+app.use(cors(
+    {
+        origin:"http://localhost:5173",
+        credentials:true
+    }
+))
 app.use("/api",router);
 
 app.listen(3000,()=>{
